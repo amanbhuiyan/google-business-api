402977ea3fa479ec126cc30352cd220f56338bc9
Aman <aub204@yahoo.com>
2023-10-18 13:34:32 +0100

    wsgi file location back to original place-25

--- googleBusinessProject/googleBusinessProject/wsgi.py
+++ googleBusinessProject/googleBusinessProject/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.googleBusinessProject.settings')
+
 
 
 
