3631b03c9ccdf656cafae0ab3ef03decdd362ba5
Aman <aub204@yahoo.com>
2023-10-18 13:23:34 +0100

    wsgi file change settings- 23

--- googleBusinessProject/googleBusinessProject/wsgi.py
+++ googleBusinessProject/googleBusinessProject/wsgi.py
@@ -11,7 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'googleBusinessProject.googleBusinessProject.settings')
+
+
+
 
 
 application = get_wsgi_application()
